4993df1b744f715b4edadeca0e4a53912ec022ec
Alexandre COIN <alexandre.pc.coin@gmail.com>
2025-02-10 10:37:12 +0100

    tech: api: revert tech-improve-pgboss-usage
    This reverts commit 26acacaa2db06177080a5ee3ec7c303bffd415b0.

--- api/src/evaluation/infrastructure/repositories/answer-job-repository.js
+++ api/src/evaluation/infrastructure/repositories/answer-job-repository.js
@@ -1,5 +1,6 @@
 import { AnswerJob } from '../../../quest/domain/models/AnwserJob.js';
 import { config } from '../../../shared/config.js';
+import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { temporaryStorage } from '../../../shared/infrastructure/key-value-storages/index.js';
 import { JobRepository } from '../../../shared/infrastructure/repositories/jobs/job-repository.js';
 
@@ -18,8 +19,17 @@ export class AnswerJobRepository extends JobRepository {
   async performAsync(job) {
     if (!config.featureToggles.isAsyncQuestRewardingCalculationEnabled || !config.featureToggles.isQuestEnabled) return;
 
-    await super.performAsync(job);
-    await this.#profileRewardTemporaryStorage.increment(job.userId);
+    const knexConn = DomainTransaction.getConnection();
+
+    if (knexConn.isTransaction) {
+      await super.performAsync(job);
+      await this.#profileRewardTemporaryStorage.increment(job.userId);
+    } else {
+      await DomainTransaction.execute(async () => {
+        await super.performAsync(job);
+        await this.#profileRewardTemporaryStorage.increment(job.userId);
+      });
+    }
   }
 }
 
--- api/tests/evaluation/unit/infrastructure/repositories/answer-job-repository_test.js
+++ api/tests/evaluation/unit/infrastructure/repositories/answer-job-repository_test.js
@@ -6,7 +6,6 @@ import { expect, sinon } from '../../../../test-helper.js';
 describe('Evaluation | Unit | Infrastructure | Repositories | AnswerJobRepository', function () {
   beforeEach(function () {
     sinon.stub(config, 'featureToggles');
-    sinon.stub(pgBoss, 'insert').resolves([]);
     config.featureToggles.isQuestEnabled = true;
     config.featureToggles.isAsyncQuestRewardingCalculationEnabled = true;
   });
@@ -15,6 +14,7 @@ describe('Evaluation | Unit | Infrastructure | Repositories | AnswerJobRepositor
     it('should do nothing if quests are disabled', async function () {
       // given
       const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
+      sinon.stub(pgBoss, 'insert').resolves([]);
       config.featureToggles.isQuestEnabled = false;
       const userId = Symbol('userId');
       const answerJobRepository = new AnswerJobRepository({
@@ -57,6 +57,48 @@ describe('Evaluation | Unit | Infrastructure | Repositories | AnswerJobRepositor
 
       // then
       expect(profileRewardTemporaryStorageStub.increment).to.have.been.calledWith(userId);
+    });
+
+    describe('should use transaction in all cases', function () {
+      it('should use existing transaction', async function () {
+        // given
+        const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
+        const knexStub = { batchInsert: sinon.stub().resolves([]), isTransaction: true };
+        sinon.stub(DomainTransaction, 'getConnection').returns(knexStub);
+        sinon.stub(DomainTransaction, 'execute').callsFake((callback) => {
+          return callback();
+        });
+        const userId = Symbol('userId');
+        const answerJobRepository = new AnswerJobRepository({
+          dependencies: { profileRewardTemporaryStorage: profileRewardTemporaryStorageStub },
+        });
+
+        // when
+        await answerJobRepository.performAsync({ userId });
+
+        // then
+        expect(DomainTransaction.execute).to.have.not.been.called;
+      });
+
+      it('should create new transaction', async function () {
+        // given
+        const profileRewardTemporaryStorageStub = { increment: sinon.stub() };
+        const knexStub = { batchInsert: sinon.stub().resolves([]), isTransaction: false };
+        sinon.stub(DomainTransaction, 'getConnection').returns(knexStub);
+        sinon.stub(DomainTransaction, 'execute').callsFake((callback) => {
+          return callback();
+        });
+        const userId = Symbol('userId');
+        const answerJobRepository = new AnswerJobRepository({
+          dependencies: { profileRewardTemporaryStorage: profileRewardTemporaryStorageStub },
+        });
+
+        // when
+        await answerJobRepository.performAsync({ userId });
+
+        // then
+        expect(DomainTransaction.execute).to.have.been.called;
+      });
     });
   });
 });
