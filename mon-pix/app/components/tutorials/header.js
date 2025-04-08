827baad9d35e45c4cc0567dcd8a015a328319e4e
Eric Lim <eric.lim@pix.fr>
2025-04-08 11:22:41 +0200

    wip
    Co-authored-by: Theo <theotime.berthod@pix.fr>

--- mon-pix/app/components/tutorials/header.js
+++ mon-pix/app/components/tutorials/header.js
@@ -1,6 +1,21 @@
+import { action } from '@ember/object';
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class Header extends Component {
   @service featureToggles;
+  @service router;
+
+  @tracked filterToggleSwitched;
+
+  constructor(...args) {
+    super(...args);
+    this.filterToggleSwitched = this.router.currentRouteName === 'authenticated.user-tutorials.recommended';
+  }
+
+  @action
+  switchToggle() {
+    this.filterToggleSwitched = !this.filterToggleSwitched;
+  }
 }
