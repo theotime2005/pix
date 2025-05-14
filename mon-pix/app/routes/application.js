982fdb70a99151de677dd27389d911aae7b97228
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-05-14 18:03:28 +0200

    feat-i18n-1

--- mon-pix/app/routes/application.js
+++ mon-pix/app/routes/application.js
@@ -3,6 +3,9 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import ENV from 'mon-pix/config/environment';
 
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 export default class ApplicationRoute extends Route {
   @service authentication;
   @service featureToggles;
@@ -26,6 +29,38 @@ export default class ApplicationRoute extends Route {
     await this.oidcIdentityProviders.load().catch();
 
     await this.authentication.handleAnonymousAuthentication(transition);
+
+    i18next.use(LanguageDetector).init({
+      supportedLngs: ['en', 'fr', 'fr-FR', 'fr-BE', 'nl-BE'],
+      // order and from where user language should be detected
+      order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+      // keys or params to lookup language from
+      lookupQuerystring: 'lng',
+      lookupCookie: 'i18next',
+      lookupLocalStorage: 'i18nextLng',
+      lookupSessionStorage: 'i18nextLng',
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+
+      // cache user language on
+      caches: ['localStorage', 'cookie'],
+      excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+      // optional expiry and domain for set cookie
+      cookieMinutes: 10,
+      cookieDomain: 'myDomain',
+
+      // optional htmlTag with lang attribute, the default is:
+      htmlTag: document.documentElement,
+
+      // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+      cookieOptions: { path: '/', sameSite: 'strict' },
+
+      // optional conversion function used to modify the detected language code
+      convertDetectedLanguage: 'Iso15897',
+      convertDetectedLanguage: (lng) => lng.replace('-', '_'),
+    });
 
     await this.session.handleUserLanguageAndLocale(transition);
   }
