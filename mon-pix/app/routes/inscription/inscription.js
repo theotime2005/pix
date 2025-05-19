91ac65594e372e1c09b0a70d168a71c15fe377cb
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2025-05-19 09:37:07 +0200

    feat(mon-pix): allow access to sign-in page for authenticated anonymous user

--- mon-pix/app/routes/inscription/inscription.js
+++ mon-pix/app/routes/inscription/inscription.js
@@ -1,22 +1,38 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import get from 'lodash/get';
+
 
 export default class InscriptionRoute extends Route {
   @service session;
   @service store;
+  @service currentUser;
+
+  hasAnonymousUserId = false; // propriété de classe
 
   beforeModel() {
-    this.session.prohibitAuthentication('authenticated.user-dashboard');
+    this.hasAnonymousUserId = get(this.session, 'data.authenticated.user_id') !== undefined;
+    console.log(this.hasAnonymousUserId);
+    if (this.session.isAuthenticated && !this.hasAnonymousUserId) {
+      console.log("authentication prohibited");
+      this.session.prohibitAuthentication('authenticated.user-dashboard');
+    }
   }
 
   model() {
     // XXX: Model needs to be initialize with empty to handle validations on all fields from Api
-    return this.store.createRecord('user', {
-      lastName: '',
-      firstName: '',
-      email: '',
-      password: '',
-      cgu: false,
-    });
+    if (this.hasAnonymousUserId) {
+      // User anonyme déjà existant : on veut le compléter
+      console.log(this.currentUser.user);
+      return this.currentUser.user; //cet objet doit contenir le token (avec la route /users/me)
+    } else {
+      return this.store.createRecord('user', {
+        lastName: '',
+        firstName: '',
+        email: '',
+        password: '',
+        cgu: false,
+      });
+    }
   }
 }
