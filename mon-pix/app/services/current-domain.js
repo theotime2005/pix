7e2dab85d165de8a96a350d65035c7ccc5f56148
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-07-17 12:49:22 +0200

    feat(mon-pix): add getDomain fonction to currentDomain service

--- mon-pix/app/services/current-domain.js
+++ mon-pix/app/services/current-domain.js
@@ -1,12 +1,27 @@
-import Service from '@ember/service';
+import Service, { service } from '@ember/service';
 import last from 'lodash/last';
 import PixWindow from 'mon-pix/utils/pix-window';
 
 const FRANCE_TLD = 'fr';
 
 export default class CurrentDomainService extends Service {
+  @service location;
+
   getExtension() {
     return last(PixWindow.getLocationHostname().split('.'));
+  }
+
+  getDomain() {
+    const url = new URL(this.location.getUrl())
+    let domain;
+
+    if (url.hostname == 'localhost') {
+      domain = url.host;
+    } else {
+      domain = url.host.split('.').slice(-2).join('.')
+    }
+
+    return domain;
   }
 
   get isFranceDomain() {
