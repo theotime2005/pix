776830bff2698ace026faca3836b34e606f2ef8c
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-07-17 12:48:39 +0200

    feat(mon-pix): add getUrl fonction to location service

--- mon-pix/app/services/location.js
+++ mon-pix/app/services/location.js
@@ -1,6 +1,10 @@
 import Service from '@ember/service';
 
 export default class LocationService extends Service {
+  getUrl() {
+    return window.location.href;
+  }
+
   replace(url) {
     window.location.replace(url);
   }
