4d2c77b7ccbefc01830dcaa0c527dce812170069
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-07-22 10:50:59 +0200

    chore(api): rename combined course participation route

--- api/src/quest/routes.js
+++ api/src/quest/routes.js
@@ -1,7 +1,7 @@
-import * as questParticipationRoute from './application/combined-course-route.js';
+import * as combinedCourseRoute from './application/combined-course-route.js';
 import * as questRoute from './application/quest-route.js';
 import * as verifiedCodeRoute from './application/verified-code-route.js';
 
-const questRoutes = [questParticipationRoute, questRoute, verifiedCodeRoute];
+const questRoutes = [combinedCourseRoute, questRoute, verifiedCodeRoute];
 
 export { questRoutes };
